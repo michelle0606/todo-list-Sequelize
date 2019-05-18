187f1690c29264ff40d678e13452b1a7bee88257
walle <michelle210181@gmail.com>
2019-05-18 10:57:20 +0800

    add home and user routers

--- app.js
+++ app.js
@@ -1,52 +1,27 @@
-const express = require("express");
-const app = express();
-const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 // 載入 model
-const db = require("./models");
-const Todo = db.Todo;
-const User = db.User;
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
-// 設定路由
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-// 認證系統的路由
-// 登入頁面
-app.get("/users/login", (req, res) => {
-  res.render("login");
-});
-// 登入檢查
-app.post("/users/login", (req, res) => {
-  res.send("login");
-});
-// 註冊頁面
-app.get("/users/register", (req, res) => {
-  res.render("register");
-});
-// 註冊檢查
-app.post("/users/register", (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect("/"));
-});
-// 登出
-app.get("/users/logout", (req, res) => {
-  res.send("logout");
-});
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
-// 設定 express port 3000
+app.use('/users', require('./routes/user'))
+
 app.listen(3000, () => {
-  db.sequelize.sync();
-  console.log(`App is running!`);
-});
+  db.sequelize.sync()
+  console.log(`App is running!`)
+})
